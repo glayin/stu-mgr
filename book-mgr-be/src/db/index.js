7d8e739e881ff06b849fc249258e361d93bd0f25
glayin <llx98@foxmail.com>
2022-04-17 12:53:50 +0800

    initialization

---
+++ book-mgr-be/src/db/index.js
@@ -0,0 +1,35 @@
+
+const mongoose = require('mongoose')
+
+//哪个数据库
+//哪个集合
+//添加什么格式的文档
+
+//Schema映射了MongoDB下的一个集合，并且它的内容就是集合下的文档
+//Modal可以理解成根据Schema生成的一套方法，这套方法用来操作MongoDB下的集合和集合下的文档
+
+const UserSchema = new mongoose.Schema({
+    nickname: String,
+    password: String,
+    age: Number
+})
+
+const UserModal = mongoose.model('User', UserSchema)
+
+const  connect = () => {
+    //连接数据库
+    mongoose.connect('mongodb://127.0.0.1:27017/book-mgr')
+    //数据库打开时做一些事情
+    mongoose.connection.on('open', () => {
+        console.log('连接成功')
+        const user = new UserModal({
+            nickname:'小明',
+            password: '123456',
+            age:12
+        })
+        user.save()
+    })
+
+}
+
+connect()
---
+++ book-mgr-be/src/index.js
@@ -0,0 +1,39 @@
+const Koa = require('koa')
+
+const app = new Koa()
+//通过app.use注册中间件
+//中间件本质上是一个函数
+//context上下文-当前请求的信息都在里面
+app.use((context)=>{
+    const {request : req } = context
+    const {url} = req
+
+    if(url === '/user'){
+        context.body = 'abcde'
+        return
+    }
+
+    context.body = '??'
+})
+
+const utils = require('./helpers/utils/index')
+
+console.log(utils)
+
+// app.use((ctx) => {
+//     const {path = '/'} = ctx
+//
+//     if(path === '/user/123'){
+//         ctx.body = '返回用户123的信息'
+//     }
+//     if(path === '/settings'){
+//         ctx.body = "返回一些设置的信息"
+//     }
+// })
+
+//开启一个http服务
+//接收http请求并作处理，处理完后响应
+//默认端口http80 ， https 443
+app.listen(3000, () => {
+    console.log('启动成功')
+})
